ca82bcb632d2e0ddbe504815451b8763e924024a
deepika <deepika.thiru@wallstreetenglish.com>
2019-03-04 13:01:41 +0530

    New changelist

--- src/amazon/BaseScreen.java
+++ src/amazon/BaseScreen.java
@@ -17,5 +17,4 @@ public class BaseScreen {
 		this.driver = inputDriver;
 		PageFactory.initElements(driver, this);
 		System.our.println("hi lets meake a sample test with no changes another test");
-	}
-}
+		System.out.println("last but not the least");
