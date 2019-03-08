9837f4691ce4778560f5f3abf6f36685208e2b95
deepikathiru <testLearn@123>
2019-03-08 14:49:31 +0530

    test deepika

--- src/amazon/BaseScreen.java
+++ src/amazon/BaseScreen.java
@@ -1,23 +1,7 @@
 package amazon;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 
 public class BaseScreen {
 
-	public WebDriver driver;
 
-	/*
-	 *This method is used to initiate the elements using page factory
-	 */
-
-	public BaseScreen(WebDriver inputDriver)
-	{
-
-		this.driver = inputDriver;
-		PageFactory.initElements(driver, this);
-		System.our.println("hi lets meake a sample test with no changes another test");
-		System.out.println("last but not the least test");
-		System.our.println("hi lets meake a sample test with no changes another test with");
-	}
 }
